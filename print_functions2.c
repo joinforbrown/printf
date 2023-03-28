f6182fafba81ba087182e23ab7ce8ac28421ace1
haveThe <110726621+havecodesoft@users.noreply.github.com>
2023-03-29 02:10:02 +0300

    Update print_functions2.c

--- print_functions2.c
+++ print_functions2.c
@@ -9,7 +9,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed.
+ * Return: Number of chars printed
  */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -52,7 +52,7 @@ int print_pointer(va_list types, char buffer[],
 		width, flags, padd, extra_c, padd_start));
 }
 
-/************************* PRINT NON PRINTABLE *************************/
+/*********************** PRINT NON PRINTABLE ***********************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
  * @types: Lista of arguments
@@ -135,7 +135,7 @@ int print_reverse(va_list types, char buffer[],
 	}
 	return (count);
 }
-/************************* PRINT A STRING IN ROT13 *************************/
+/*********************** PRINT A STRING IN ROT13 ***********************/
 /**
  * print_rot13string - Print a string in rot13.
  * @types: Lista of arguments

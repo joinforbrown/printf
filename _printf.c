fa0deae0b4c3163fc9097fd87171217dc4807a68
haveThe <110726621+havecodesoft@users.noreply.github.com>
2023-03-29 01:53:47 +0300

    Update print.f

--- _printf.c
+++ _printf.c
@@ -7,6 +7,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  * Return: Printed chars.
  */
+ 
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -60,7 +61,9 @@ int _printf(const char *format, ...)
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
+	{
 		write(1, &buffer[0], *buff_ind);
+	}
 
 	*buff_ind = 0;
 }

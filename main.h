460c4d3ca733ecf5b82c7f52adbd6d52e74d157d
haveThe <110726621+havecodesoft@users.noreply.github.com>
2023-03-29 02:05:52 +0300

    Update main.h

--- main.h
+++ main.h
@@ -22,7 +22,7 @@
  * struct fmt - Struct op
  *
  * @fmt: The format.
- * @fn: The function associated.
+ * @fn: The function associated
  */
 struct fmt
 {
@@ -35,7 +35,7 @@ struct fmt
  * typedef struct fmt fmt_t - Struct op
  *
  * @fmt: The format.
- * @fm_t: The function associated.
+ * @fm_t: The function associated
  */
 typedef struct fmt fmt_t;
 
@@ -114,5 +114,5 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* MAIN_H */
+#endif
 
